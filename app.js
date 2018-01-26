352f26fba8429cf9713bdc0fabcbab4a66183bee
OmarNizam <eng.omarnizam@gmail.com>
2018-01-26 17:21:09 +0100

    Get all the trachings for postnl

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
+'use strict';
+
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const jsonParser = require('body-parser').json
+//const cors = require('cors')
+
+const Aftership = require('aftership')('06979555-b9a4-46e9-babc-de5b3eb55579')
 
 //const { shipments } = require('./routes')
 const PORT = process.env.PORT || 3030
 
 let app = express()
-  .use(cors())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json())
+  //.use(cors())
+  //.use(bodyParser.urlencoded({ extended: true }))
+//  .use(bodyParser.json())
   // Our routes
   //.use(shipments)
+app.use(jsonParser())
 
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+
+
+  // Aftership.GET('/trackings', function(req, res) {
+  //   var body = res.data
+  //   var trackings = body.trackings[0].checkpoints
+  //
+  //   trackings = JSON.stringify(trackings)
+  //
+  //   //(trackings)=> { res.json(trackings)}
+  //
+  //   //trackings = trackings.sort({ created_at: -1 })
+  //
+  //
+  //   console.log(trackings)
+  //   })
+
+app.get('/trackings',(req, res, next) => {
+  let query = {
+  	slug: 'postnl'
+  };
+
+  Aftership.call('GET', '/trackings', query)
+    .then(function(result) {
+      let trackings = result.data.trackings
+      console.log(trackings)
+      res.json(trackings)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
 })
 
 // catch 404 and forward to error handler
---
+++ router.js
@@ -0,0 +1,12 @@
+
+
+
+const router = require('express').Router()
+
+// Aftership.GET('/path', options, callback);
+
+
+
+
+
+module.exports = router
